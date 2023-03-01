0ec942383363eb5c42ae7bb3425e41a378cc7370
mcgraneder <mcgrane480@gmail.com>
2023-03-01 02:29:15 +0000

    chore address

--- src/pages/address/[address].tsx
+++ src/pages/address/[address].tsx
@@ -1,12 +1,5 @@
-import type { GetStaticPaths, NextPage } from "next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import type { NextPage } from "next";
 import { Layout } from "src/layouts";
-import { useViewport } from "../../hooks/useViewport";
-import useEthRPC from "../../hooks/useRPC";
-import { useState, useEffect } from "react";
-import { useRecentBlocks } from "../../hooks/useRecentBlocks";
-import { useRecentTransactions } from "../../hooks/useRecentTransactions";
-import useInterval from "use-interval";
 import AddressData from "../../components/explorer/Addresses/AddressData";
 import { LoadingIndicator } from "../../components/icons/LoadingIndicator";
 import { useRPCClient } from "src/contexts/useRPCClient";
@@ -22,19 +15,6 @@ const BlockPage: NextPage = () => {
       </Layout>
     </>
   );
-};
-
-export const getStaticProps = async ({ locale }: any) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ["common", "errors"])),
-  },
-});
-
-export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
-  return {
-    paths: [],
-    fallback: "blocking",
-  };
 };
 
 export default BlockPage;
